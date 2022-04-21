fba6d3e4baff03cb7b9f7b136ff140a029bdfa2b
charlesbrown502 <83849147+charlesbrown502@users.noreply.github.com>
2022-04-21 13:02:00 +0530

    Update fsub.py

--- Tools/fsub.py
+++ Tools/fsub.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters
+from pyrogram import Client, Filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from config import Config
