b3c14848b79a4a13a45280b7ff93a829df9a1208
charlesbrown502 <83849147+charlesbrown502@users.noreply.github.com>
2022-04-21 20:25:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,9 +11,8 @@ from Tools.Download import download
 #from Tools import fsub
 
 
-@Client.on_message(Filters.command(["start"]))
+@Client.on_message(Filters.private & Filters.command(["start"]))
 async def start(c, m):
-
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.START.format(m.from_user.first_name),
                          reply_to_message_id=m.message_id,
@@ -22,45 +21,32 @@ async def start(c, m):
 
 
 
-@Client.on_message(Filters.command(["help"]))
+@Client.on_message(Filters.private & Filters.command(["help"]))
 async def help(c, m):
-
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.HELP,
                          reply_to_message_id=m.message_id,
                          parse_mode="markdown")
 
 
-@Client.on_message(Filters.command(["about"]))
+@Client.on_message(Filters.private & Filters.command(["about"]))
 async def about(c, m):
-
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.ABOUT,
                          disable_web_page_preview=True,
                          reply_to_message_id=m.message_id,
                          parse_mode="markdown")
 
-@Client.on_message(Filters.command(["converttovideo"]))
+@Client.on_message(Filters private & Filters.command(["converttovideo"]))
 async def video(c, m):
-    
-  if m.from_user.id in Config.BANNED_USER:
-      await c.send_message(chat_id=m.chat.id, text=Translation.BANNED_TEXT)
-      return
-  if m.from_user.id not in Config.BANNED_USER:
-      if m.reply_to_message is not None:
-          #await must_join_channel(c, m)
-          await download(c, m)
-      else:
-          await c.send_message(chat_id=m.chat.id, text=Translation.REPLY_TEXT)
-
-@Client.on_message(Filters.command(["converttofile"]))
-async def file(c, m):
-    
-  if m.from_user.id in Config.BANNED_USER:
-      await c.send_message(chat_id=m.chat.id, text=Translation.BANNED_TEXT)
-  if m.from_user.id not in Config.BANNED_USER:
     if m.reply_to_message is not None:
-      #await must_join_channel(c, m)
-      await download(c, m)
+        await download(c, m)
     else:
-       await c.send_message(chat_id=m.chat.id, text=Translation.REPLY_TEXT)
+        await c.send_message(chat_id=m.chat.id, text=Translation.REPLY_TEXT)
+
+@Client.on_message(Filters.private & Filters.command(["converttofile"]))
+async def file(c, m):
+  if m.reply_to_message is not None:
+        await download(c, m)
+    else:
+        await c.send_message(chat_id=m.chat.id, text=Translation.REPLY_TEXT)
