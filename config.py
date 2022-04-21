610161cafcd721e96ee6ec7e1b2522aa311b0060
charlesbrown502 <83849147+charlesbrown502@users.noreply.github.com>
2022-04-21 20:21:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     #AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-    BANNED_USER = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+    #BANNED_USER = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-    MAX_MESSAGE_LENGTH = 4096
     DB_URI = os.environ.get("DATABASE_URL", "")
