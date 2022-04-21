a33d15a0f0386e5d6c923012e7006f3baf46ffa8
charlesbrown502 <83849147+charlesbrown502@users.noreply.github.com>
2022-04-21 12:45:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -17,7 +17,7 @@ async def start(c, m):
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.START.format(m.from_user.first_name),
                          reply_to_message_id=m.message_id,
-                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates Channel", url="t.me/xz_bots), InlineKeyboardButton("Support", url="t.me/xz_supportbot)]]))
+                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates Channel", url="t.me/xz_bots"), InlineKeyboardButton("Support", url="t.me/xz_supportbot")]]))
     logger.info(f"{m.from_user.first_name} used start command")
 
 
